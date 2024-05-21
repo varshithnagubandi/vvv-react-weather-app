d5ea921ca49c43da5ae89d03104a1c4ec277386f
vvvarshith <vvvarshith.nagubandi@gmail.com>
2024-05-21 12:56:58 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState("");
+
+  //to store the temperature
+  const [result, setResult] = useState("");
+
+  const changeHandler = (e) => {
+    setCity(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const kelvin = data.main.temp;
+        const celsius = kelvin - 273.15;
+        //console.log(celsius);
+        //Math.round() used to remove the decimal places
+        setResult(
+          "TEMPERATURE AT " + " " + city + "\n" + Math.round(celsius) + "°C"
+        );
+        setCity("");
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <div className="card">
+        <div className="card-body">
+          <h4 className="title">WEATHER APP</h4>
+          <form onSubmit={submitHandler}>
+            <input
+              type="text"
+              name="city"
+              placeholder="ENTER CITY"
+              value={city}
+              onChange={changeHandler}
+            />
+            <br />
+            <br />
+            <input type="submit" value="GET TEMPERATURE" />
+          </form>
+          <h1>{result}</h1>
+        </div>
+      </div>
+    </center>
   );
-}
+};
 
 export default App;
